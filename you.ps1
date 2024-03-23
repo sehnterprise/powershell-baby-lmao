0b1b807b919a79b7cccb38b1ea277a922f1abae8
Daniel Sehn <164395922+sehnterprise@users.noreply.github.com>
2024-03-23 15:28:13 -0400

    Let's Get Ready To Get Weird
    weird....

---
+++ you.ps1
@@ -0,0 +1,15 @@
+Get-Command -noun service
+Get-Help Get-Service -full
+
+$number1=12
+$number2=21
+$number3=$number2+$number1
+$intro = "exit "
+
+$final= $intro + [string]$number3
+$final
+
+$CanRun=$true
+$CanRun
+$CanRun.GetType()
+Get-Date
